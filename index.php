ac3ef03ffa1557c471bbb6d2663584cd182bae00
trayectophp2024 <trayectophp2024@gmail.com>
2024-11-25 11:38:01 -0300

    fix navbar and footer

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-require('partials/header.php');
+
+require_once 'functions/autoload.php';
 
 $secciones_validas = [
     "home" => [
@@ -57,6 +58,50 @@ if (!array_key_exists($seccion, $secciones_validas)) {
 
 </head>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="pcolor container-fluid">
+
+        <a class="navbar-brand pcolore" href="index.php?sec=home">
+            Kiki&LalaShop.
+        </a>
+        <a href="index.php?sec=todos" class="icono">
+            <i class="fa-solid"> <img width="50px" src="img/kklltransp.png" alt=""></i>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="pcolores nav-link" aria-current="page" href="index.php?sec=home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=categorias&categoria=makeup">Makeup</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=categorias&categoria=skincare">Skincare</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=categorias&categoria=accesorios">Accesorios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=nosotras">Nosotras</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=envios">Envios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=contacto">Contacto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="pcolores nav-link" href="index.php?sec=creador">Creador</a>
+                </li>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <body>
 
     <main class="container">
@@ -65,8 +110,14 @@ if (!array_key_exists($seccion, $secciones_validas)) {
         ?>
 
     </main>
+
+
+    <footer style="background-color:#FDB0C0;">
+        <p class="text-light text-center p-4">Todos los derechos reservados - 2024 - Kiki&LalaShop</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
-<?php
-require('partials/footer.php');
-?>
+
 </html>
--- partials/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="background-color:#FDB0C0;">
-     <p class="text-light text-center p-4">Todos los derechos reservados - 2024 - Kiki&LalaShop</p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
