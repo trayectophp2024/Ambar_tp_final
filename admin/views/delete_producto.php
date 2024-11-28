4ce071cb147fe58f7095fe431657e84f25198d32
trayectophp2024 <trayectophp2024@gmail.com>
2024-11-28 11:24:10 -0300

    fix delete

--- admin/views/delete_producto.php
+++ admin/views/delete_producto.php
@@ -9,5 +9,5 @@
 
 <div class="row my-5 g-3">
     <h1>¿Está seguro que desea eliminar el producto : <?=$proucto->getNombre() ?> ?</h1>
-    <button style="background-color:#f1666d" class="btn" ><a href="actions/delete_productos_acc.php?id=<?=$proucto->getId() ?>" class="text-decoration-none text-black">si, eliminar</a></button>
+    <button style="background-color:#f1666d" class="btn" ><a href="actions/delete_producto_acc.php?id=<?=$proucto->getId() ?>" class="text-decoration-none text-black">si, eliminar</a></button>
 </div>
